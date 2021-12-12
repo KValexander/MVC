e7323b14aae4e8a444eea644aed9ad626687c2b3
KValexander <ewoase@gmail.com>
2021-12-12 11:34:42 +0300

    checks upd

--- core/helpers/checks.php
+++ core/helpers/checks.php
@@ -3,6 +3,8 @@
 	function slash_check($str) {
 		$length = strlen($str);
 		if($length <= 1) return $str;
+		$first = substr($str, 0, 1);
+		if($first != "/") $str = "/". $str;
 		$last = substr($str, -1);
 		if($last == "/") return substr($str, 0, -1);
 		else return $str;

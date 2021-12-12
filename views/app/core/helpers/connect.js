1fbcb3a1e6b2cf7b1750406f095f25d055214bf5
KValexander <ewoase@gmail.com>
2021-12-12 17:59:20 +0300

    connect upd

--- views/app/core/helpers/connect.js
+++ views/app/core/helpers/connect.js
@@ -7,16 +7,32 @@ let connect = {
 			array.forEach(path => connect.create(path, "script"))
 		else connect.create(array, "script")
 	},
+	// Connecting styles
+	style: function(array) {
+		if(Array.isArray(array))
+			array.forEach(path => connect.create(path, "link"))
+		else connect.create(array, "link")
+	},
 	// Generating the connection code
 	create: function(path, type) {
 		if(!connect.check(path)) return;
 		// Checking if a script exists
 		if(document.getElementById(path)) return;
 		element = document.createElement(type);
-		element.type = (type == "script") ? "text/javascript" : "";
+		if(type == "script") {
+			element.type = "text/javascript";
+			element.src = path;
+		} else if(type == "link") {
+			element.rel = "stylesheet";
+			element.href = path;
+		}
 		element.id = path;
-		element.src = path;
 		document.querySelector("head").appendChild(element);
+	},
+	// Delete connection
+	remove: function(path) {
+		console.log(path);
+		document.getElementById(path).remove();
 	},
 	// Checking for file existence
 	check: function(path) {
